c01aef9ca5a0f3d6c7af04f9a233ad993415b415
2004Ljl <1459780510@qq.com>
2025-06-20 02:45:19 +0000

    [automatic commit] 2025-06-20 02:45:19 UTC

--- part1/db.js
+++ part1/db.js
@@ -1,9 +1,6 @@
 const mysql = require('mysql2/promise');
 
 const database = mysql.createPool({
-    // host: 'localhost',
-    // user: 'webuser',
-    // password: '123456',
     database: 'DogWalkService'
 });
 
