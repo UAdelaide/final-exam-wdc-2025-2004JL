29d59db38f6ee490e224f25849623af5f2b8bec8
2004Ljl <1459780510@qq.com>
2025-06-20 03:06:53 +0000

    [automatic commit] 2025-06-20 03:06:53 UTC

--- part1/app.js
+++ part1/app.js
@@ -26,10 +26,10 @@ async function inputTestData() {
         await database.execute(`
             INSERT INTO Users (username, email, password_hash, role) VALUES
             ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-    ('bobwalker', 'bob@example.com', ' hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('ljl89', 'ljl@example.com', 'hashedabc', 'walker'),
-('sun567', 'sun@example.com', 'hashedxyz', 'owner');
+            ('bobwalker', 'bob@example.com', ' hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+            ('ljl89', 'ljl@example.com', 'hashedabc', 'walker'),
+            ('sun567', 'sun@example.com', 'hashedxyz', 'owner');
             `)
     }
 
