41e01e361ec8579ced1fb80a207b25945b797fa3
2004Ljl <1459780510@qq.com>
2025-06-20 10:49:50 +0000

    [automatic commit] 2025-06-20 10:49:50 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,6 +72,7 @@ router.post('/login', async (req, res) => {
 // question 14
 // student add logout
 router.post('/logout', (req,res) =>{
+  
   if (err) {
     return res.status(500).json({error: 'logout failed'});
   }
