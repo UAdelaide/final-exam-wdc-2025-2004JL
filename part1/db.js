dc8f44e71fcc0eff7b12c68f9b35e6dbe112c010
2004Ljl <1459780510@qq.com>
2025-06-20 01:28:42 +0000

    [automatic commit] 2025-06-20 01:28:42 UTC

---
+++ part1/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const database = mysql.createPool({
+    host: 'localhost',
+    user: 'webuser',
+    password: '123456',
+    database: 'login'
+});
+
+module.exports = database;
