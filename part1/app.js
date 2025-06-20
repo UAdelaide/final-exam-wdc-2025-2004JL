901646dcca3ed42c00f4e39ceedc6719c5ff0c57
2004Ljl <1459780510@qq.com>
2025-06-20 03:19:36 +0000

    [automatic commit] 2025-06-20 03:19:36 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/app.js
+++ part1/app.js
@@ -56,7 +56,7 @@ async function inputTestData() {
             (
             (SELECT request_id FROM WalkRequests WHERE dog_id =(SELECT dog_id FROM Dogs WHERE name = 'Max')),
             (SELECT user_id FROM Users WHERE username = 'bobwalker'),
-            (SELECT user_id FROM Users WHERE username = 'alice123')`),
+            (SELECT user_id FROM Users WHERE username = 'alice123')
             5,
             'good walk!'
             ),
