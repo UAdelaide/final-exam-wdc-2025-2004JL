b32cd630e2b7ba07f2209c213b5f99a10cacf571
2004Ljl <1459780510@qq.com>
2025-06-20 03:25:45 +0000

    [automatic commit] 2025-06-20 03:25:45 UTC

--- part1/app.js
+++ part1/app.js
@@ -56,21 +56,21 @@ async function inputTestData() {
             (
             (SELECT request_id FROM WalkRequests WHERE dog_id =(SELECT dog_id FROM Dogs WHERE name = 'Max')),
             (SELECT user_id FROM Users WHERE username = 'bobwalker'),
-            (SELECT user_id FROM Users WHERE username = 'alice123')
+            (SELECT user_id FROM Users WHERE username = 'alice123'),
             5,
             'good walk!'
             ),
             (
             (SELECT request_id FROM WalkRequests WHERE dog_id =(SELECT dog_id FROM Dogs WHERE name = 'Bella')),
             (SELECT user_id FROM Users WHERE username = 'bobwalker'),
-            (SELECT user_id FROM Users WHERE username = 'carol123')
+            (SELECT user_id FROM Users WHERE username = 'carol123'),
             4,
             'nice!'
             ),
             (
             (SELECT request_id FROM WalkRequests WHERE dog_id =(SELECT dog_id FROM Dogs WHERE name = 'Mike')),
             (SELECT user_id FROM Users WHERE username = 'ljl89'),
-            (SELECT user_id FROM Users WHERE username = 'sun567')
+            (SELECT user_id FROM Users WHERE username = 'sun567'),
             5,
             'happy!'
             );`
