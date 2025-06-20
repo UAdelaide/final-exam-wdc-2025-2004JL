70831217942b5ec6ef37150e5f955cc074d350fd
2004Ljl <1459780510@qq.com>
2025-06-20 09:01:48 +0000

    [automatic commit] 2025-06-20 09:01:48 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -52,6 +52,7 @@ router.post('/login', async (req, res) => {
 
     // stundent add
     // save user information into session
+    const user = row[0]
     
     req.session.user = {
       id: user.user_id,
