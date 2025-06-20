ac33cff9746e4761cb35e61c88a0a60ae0572760
2004Ljl <1459780510@qq.com>
2025-06-20 11:38:18 +0000

    [automatic commit] 2025-06-20 11:38:18 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,7 +87,9 @@ router.get('/mydogs', async(req,res) => {
   try {
     const [rows] = await db.query(`
       SELECT dog_id, name FROM Dogs
-      WHERE owner_id=?`, )
+      WHERE owner_id=?`, [req.session.user.id]);
+
+      res.json
   }
 })
 
